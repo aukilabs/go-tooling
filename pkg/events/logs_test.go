89ee5d867f01cdc71ac6045200942eb78da889c7
Maxence Charriere <charriere@outlook.com>
2022-06-20 13:14:42 +0200

    Events (#9)
    * events pkg implementation
    
    * event logger

---
+++ pkg/events/logs_test.go
@@ -0,0 +1,25 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/aukilabs/go-tooling/pkg/logs"
+)
+
+func TestLogger(t *testing.T) {
+	initLogs(t)
+
+	l := Logger{
+		Pusher: &Pusher{
+			Endpoint:      "",
+			BatchSize:     100,
+			FlushInterval: time.Minute,
+		},
+		Printer: t.Log,
+	}
+
+	logs.SetLogger(l.Log)
+
+	logs.New().Debug("hi")
+}
