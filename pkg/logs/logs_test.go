0e141bdacd505e073f1b03dd92b85faf7a4e669e
Ondřej Musil <ondra.musil@email.cz>
2022-09-15 15:18:34 +0900

    Fixed Do not stringify ints/floats

--- pkg/logs/logs_test.go
+++ pkg/logs/logs_test.go
@@ -1,6 +1,7 @@
 package logs
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"testing"
@@ -160,15 +161,15 @@ func TestEntryGetError(t *testing.T) {
 
 func TestEntryTags(t *testing.T) {
 	e := WithTag("hello", "max")
-	require.Equal(t, map[string]string{"hello": "max"}, e.Tags())
+	require.Equal(t, map[string]any{"hello": "max"}, e.Tags())
 }
 
-func TestToString(t *testing.T) {
+func TestNormalizeTag(t *testing.T) {
 	SetInlineEncoder()
 
-	utests := []struct {
+	testValues := []struct {
 		in  interface{}
-		out string
+		out any
 	}{
 		{
 			in:  "hello",
@@ -184,51 +185,51 @@ func TestToString(t *testing.T) {
 		},
 		{
 			in:  -42,
-			out: "-42",
+			out: -42,
 		},
 		{
 			in:  int64(-42),
-			out: "-42",
+			out: int64(-42),
 		},
 		{
 			in:  int32(-42),
-			out: "-42",
+			out: int32(-42),
 		},
 		{
 			in:  int16(-42),
-			out: "-42",
+			out: int16(-42),
 		},
 		{
 			in:  int8(-42),
-			out: "-42",
+			out: int8(-42),
 		},
 		{
 			in:  uint(84),
-			out: "84",
+			out: uint(84),
 		},
 		{
 			in:  uint64(84),
-			out: "84",
+			out: uint64(84),
 		},
 		{
 			in:  uint32(84),
-			out: "84",
+			out: uint32(84),
 		},
 		{
 			in:  uint16(84),
-			out: "84",
+			out: uint16(84),
 		},
 		{
 			in:  uint8(84),
-			out: "84",
+			out: uint8(84),
 		},
 		{
 			in:  42.42,
-			out: "42.42",
+			out: 42.42,
 		},
 		{
 			in:  float32(42.42),
-			out: "42.42",
+			out: float32(42.42),
 		},
 		{
 			in:  true,
@@ -248,9 +249,20 @@ func TestToString(t *testing.T) {
 		},
 	}
 
-	for _, u := range utests {
+	for _, u := range testValues {
 		t.Run(reflect.TypeOf(u.in).String(), func(t *testing.T) {
-			require.Equal(t, u.out, toString(u.in))
+			require.Equal(t, u.out, normalizeTag(u.in))
 		})
 	}
+}
+
+func TestLogEntryNilTagsCrash(t *testing.T) {
+	SetLogger(func(e Entry) {
+		//fmt.Printf("%s", e)
+		json.Marshal(e)
+	})
+
+	require.NotPanics(t, func() {
+		New().Error(errors.New("test").WithTag("foo", nil))
+	})
 }
